531f6f8981211715c1a7b5f43b60073fc5b910f0
valehob <valentinhobusch@gmail.com>
2024-07-24 22:24:51 +0200

    cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:3000', 'localhost:3001', 'https://my-app.com'
     else
-      origins 'https://my-app.com'
+      origins 'https://my-app.com', 'localhost:3000', 'localhost:3001'
     end
 
     resource "*",
