df5ab09f9abb978959939306ecb358e198952859
valehob <valentinhobusch@gmail.com>
2024-08-28 14:18:33 +0200

    user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
 
+    @progress = Progress.where(user: @user).joins(:topic).select("topics.name, progresses.*")
+    json = { :email => @user.email, :progress => @progress}.to_json
+
+    render json: json, status: 200
   end
 end
